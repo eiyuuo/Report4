07fdee80f6f19295769690af339c117b153358a1
yuto eimon <e185733@ie.u-ryukyu.ac.jp>
2018-11-27 01:10:32 +0900

    report4 third commit

--- src/main/ErrorSample.java
+++ src/main/ErrorSample.java
@@ -1,13 +1,6 @@
 public class ErrorSample {
     public static void main(String[] args) {
-        try {
-            String str = null;
-            System.out.println(str.length());
-        }
-        catch(Exception e) {
-            System.out.println(e);
-            System.out.println(e.getMessage());
-        }
-
+            String str = "百二十三";
+            int value =Integer.parseInt(str);
     }
 }
