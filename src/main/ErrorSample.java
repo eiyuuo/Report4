0b503150a5fa0ac5f8d5d65bfacceb2b11b9f169
yuto eimon <e185733@ie.u-ryukyu.ac.jp>
2018-11-27 00:59:33 +0900

    report4 second commit

--- src/main/ErrorSample.java
+++ src/main/ErrorSample.java
@@ -1,6 +1,13 @@
 public class ErrorSample {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+        try {
+            String str = null;
+            System.out.println(str.length());
+        }
+        catch(Exception e) {
+            System.out.println(e);
+            System.out.println(e.getMessage());
+        }
+
     }
 }
